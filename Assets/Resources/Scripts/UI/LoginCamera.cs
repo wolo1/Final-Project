26ea9c8ce8cce7cd70864f92138a00006e846492
Lipschitz <zhaoran.Wang@hotmail.com>
2021-12-17 06:25:36 +0100

    LoginUI

---
+++ Assets/Resources/Scripts/UI/LoginCamera.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginCamera : MonoBehaviour
+{
+    
+    public InputField Username;
+    public Text Hint;
+    public Button Login;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = Login.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnClick()
+    {
+        if (Username.text == "")
+        {
+
+        }
+        else
+        {
+            Debug.Log(Username.text);
+            Application.LoadLevel("Main");
+        }
+    }
+
+}
